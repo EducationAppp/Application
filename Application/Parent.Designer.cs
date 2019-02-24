ab3eb0729ac28c0592fdbabd30b49a0b65d4472f
EducationApp <apri.private.sign@gmail.com>
2019-02-25 06:19:28 +0900

    Parent.csのChangeFormで各コントロールからのイベントを受け取る

---
+++ Application/Parent.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace ApplicationEducation
+{
+    partial class Parent
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ParentPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // ParentPanel
+            // 
+            this.ParentPanel.Location = new System.Drawing.Point(0, 0);
+            this.ParentPanel.Name = "ParentPanel";
+            this.ParentPanel.Size = new System.Drawing.Size(1500, 800);
+            this.ParentPanel.TabIndex = 0;
+            // 
+            // Parent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1508, 809);
+            this.Controls.Add(this.ParentPanel);
+            this.Name = "Parent";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel ParentPanel;
+    }
+}
